d354c7840a71a693d0eda2c6110a185b52b2dba8
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-10-24 20:03:55 +0300

    default login

--- cyberlock/settings.py
+++ cyberlock/settings.py
@@ -144,6 +144,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# The default login redirect path
+LOGIN_URL = '/login/'
+
+
 # The default redirect url
 LOGOUT_REDIRECT_URL = 'home'  # Change 'home' to the desired URL name
 
