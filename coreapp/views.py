86b17efa98ef41b3625107ac145c1e293a117730
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-10-26 19:46:36 +0300

    require POST logout

--- coreapp/views.py
+++ coreapp/views.py
@@ -14,6 +14,9 @@ from .utils import save_contact_message
 from is_safe_url import is_safe_url
 from .forms import CustomUserCreationForm, TestimonialForm, ProfileForm, JobApplicationForm
 from colorama import Fore, Style
+from django.views.decorators.http import require_POST
+
+
 
 
 def is_staff_or_high_user(user):
