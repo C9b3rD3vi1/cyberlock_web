9b0efa4cdf1022d9e1dabf50443b5454db704a5e
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-11-01 23:02:58 +0300

    urls reset

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -2,6 +2,7 @@
 # from django.shortcuts import render
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 # url patterns for app
@@ -37,5 +38,11 @@ urlpatterns = [
 
     path('post/new/', views.blog_post_create, name='blog_post_create'),
     path('post/<int:pk>/edit/', views.blog_post_update, name='blog_post_update'),
+
+    # password reset url
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
