1450b5f336308e75d277a6b4666e36e754c509ba
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-11-23 00:23:11 +0300

    RichTextField

--- coreapp/models.py
+++ coreapp/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 from django.urls import reverse
 from django.utils.text import slugify
 from django.core.validators import URLValidator
-
+from ckeditor.fields import RichTextField
 
 
 # 1. User Profile (Team members or clients)
@@ -32,7 +32,8 @@ class Profile(models.Model):
 class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = RichTextField()
+    #content = models.TextField()
     image = models.ImageField(upload_to='blog_images/', blank=True)
     published_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
