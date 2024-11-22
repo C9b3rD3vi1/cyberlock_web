5a2abab5968e1a687ce1500c3714208c4c97d339
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-11-23 00:23:41 +0300

    RichTextFields

--- coreapp/models.py
+++ coreapp/models.py
@@ -32,8 +32,8 @@ class Profile(models.Model):
 class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    content = RichTextField()
-    #content = models.TextField()
+    #content = RichTextField()
+    content = models.TextField()
     image = models.ImageField(upload_to='blog_images/', blank=True)
     published_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
