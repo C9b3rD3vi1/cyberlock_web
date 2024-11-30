07bd3fa157a5874de1de1259b717306094792f8c
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-11-30 09:48:42 +0300

    js styling and functionality, responsiveness enhancement

--- static/js/typed.js
+++ static/js/typed.js
@@ -1,11 +1,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Array of colors for changing text color
+    const colors = ['#4c6ef5', '#00c6ff', '#f54291']; // Array of colors for each string
+
     new Typed('.display-4', {
         strings: ['Welcome to Cyberlock Technologies', 'Innovative Solutions for Your Digital Success'],
-        typeSpeed: 50, // Typing speed in milliseconds
+        typeSpeed: 100, // Typing speed in milliseconds
         backSpeed: 30, // Backspacing speed in milliseconds
         loop: true,    // Loops through the strings
         showCursor: true, // Display typing cursor
         cursorChar: '|', // Character used as the cursor
+        onStringTyped: (arrayPos) => {
+            // Change color when each string finishes typing
+            const element = document.querySelector('.display-4');
+            element.style.color = colors[arrayPos % colors.length];
+        }
     });
 });
