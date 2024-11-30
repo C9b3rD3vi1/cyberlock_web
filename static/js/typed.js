bc15e29b42e67fbfe3f6a8e8e36293db5c9f1fac
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-11-30 09:40:29 +0300

    javascritp intergration

---
+++ static/js/typed.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Typed('.display-4', {
+        strings: ['Welcome to Cyberlock Technologies', 'Innovative Solutions for Your Digital Success'],
+        typeSpeed: 50, // Typing speed in milliseconds
+        backSpeed: 30, // Backspacing speed in milliseconds
+        loop: true,    // Loops through the strings
+        showCursor: true, // Display typing cursor
+        cursorChar: '|', // Character used as the cursor
+    });
+});
