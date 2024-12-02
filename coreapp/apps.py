ac4874bc77f94eb6e3e561e461633467fab22ad5
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-12-02 10:58:05 +0300

    {% block title %}Careers - CyberLock Tech {% endblock %}

--- coreapp/apps.py
+++ coreapp/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coreapp'
+
+    def ready(self):
+        print("App is ready and signals are loaded")
+        import coreapp.signals  # Import the signal receiver module
--- coreapp/signals.py
+++ coreapp/signals.py
@@ -8,6 +8,7 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
+        print("Signal triggered: Creating profile...")
         Profile.objects.create(user=instance)
 
 
