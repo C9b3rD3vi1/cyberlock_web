279df04e096e4a77f0c4abed3e7a4846e8acf024
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-12-02 10:44:22 +0300

    signal receiver that is triggered when a User object is saved. Its purpose is to ensure that the associated Profile object for the User is also saved.

--- coreapp/signals.py
+++ coreapp/signals.py
@@ -11,6 +11,18 @@ def create_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 
+"""
+This function is a signal receiver that is triggered when a User object is saved.
+Its purpose is to ensure that the associated Profile object for the User is also saved.
+
+Parameters:
+- `sender`: The model class that triggered the signal. In this case, it is the `User` model.
+- `instance`: The instance of the model that triggered the signal. It is the specific `User` object that was saved.
+- `**kwargs`: Additional keyword arguments that may be passed to the signal receiver.
+
+The function does not return any value. It simply saves the associated Profile object for the given User instance.
+"""
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
