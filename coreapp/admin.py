992a41b9488e179dcd790acf5a36fefe0aa56c5b
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-12-04 12:08:53 +0300

    RichText Editor

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
 # import my models
+from django.db import models
 from . models import Project
 from . models import Testimonial, Profile, BlogPost
 from . models import Service, ContactMessage, Job, JobApplication
+from ckeditor.widgets import CKEditorWidget
+
+
+class BlogPostAdmin(admin.ModelAdmin):  # Use the regular ModelAdmin
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget},
+    }
 
 
 
@@ -13,7 +21,7 @@ from . models import Service, ContactMessage, Job, JobApplication
 admin.site.register(Project)
 admin.site.register(Testimonial)
 admin.site.register(Profile)
-admin.site.register(BlogPost)
+admin.site.register(BlogPost, BlogPostAdmin)
 admin.site.register(Service)
 admin.site.register(ContactMessage)
 admin.site.register(Job)
