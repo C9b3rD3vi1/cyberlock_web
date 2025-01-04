cd6115f23f7e147063fffdee724c3d50b15f7cc4
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-01-04 14:39:09 +0300

    code reviews

--- coreapp/models.py
+++ coreapp/models.py
@@ -1,5 +1,5 @@
 
-# my database models 
+# my database models and backend code
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
